955f3d02a6fb9c921162213bd5fa695eea0d4043
Hui Wu <zwu591@aucklanduni.ac.nz>
2025-12-03 08:38:01 +0000

    Change the format of the app. Create a new home page, with two app icon in it

---
+++ home.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(page_title="Main App", page_icon="📁")
+
+st.title("Main Dashboard")
+st.write("Choose an app to open:")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.page_link("pages/App1.py", label="Go to App 1", icon="🟩", use_container_width=True)
+
+with col2:
+    st.page_link("pages/App2.py", label="Go to App 2", icon="🟦", use_container_width=True)
