2f0471de2258acef569d30d87132dae65aed9545
Hui Wu <zwu591@aucklanduni.ac.nz>
2025-12-03 08:38:42 +0000

    Change the format of the app. Create a new home page, with two app icon in it

--- home.py
+++ home.py
@@ -8,7 +8,7 @@ st.write("Choose an app to open:")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.page_link("pages/App1.py", label="Go to App 1", icon="🟩", use_container_width=True)
+    st.page_link("pages/product_price_checker.py", label="Go to App 1", icon="🟩", use_container_width=True)
 
-with col2:
-    st.page_link("pages/App2.py", label="Go to App 2", icon="🟦", use_container_width=True)
+# with col2:
+#     st.page_link("pages/App2.py", label="Go to App 2", icon="🟦", use_container_width=True)
